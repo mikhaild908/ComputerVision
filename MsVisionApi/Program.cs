120e4e89ae1d55b52ca777a44b88ee8182884dab
mikhaild908 <mikhaild908@gmail.com>
2018-05-15 12:09:09 -0700

    first commit

---
+++ MsVisionApi/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MsVisionApi
+{
+	// Microsoft Azure Cognitive Services: Computer Vision API - Pluralsight: Eduardo Freitas
+    class Program
+    {
+		const string subscriptionKey = "c45933fd0a3e4c4a9a3006160125b7ef";
+        const string uriBase = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+
+        static void Main(string[] args)
+        {
+			//string imageFilePath = @"snow-man-3008179_640.jpg";
+			string imageFilePath = @"castaway-cay.jpg";
+
+            MakeAnalysisRequest(imageFilePath);
+
+            Console.WriteLine("\nPlease wait...\n");
+            Console.ReadLine();
+        }
+
+		public static async void MakeAnalysisRequest(string imageFilePath)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                string requestParameters = "visualFeatures=Categories,Description,Color&language=en";
+                string uri = uriBase + "?" + requestParameters;
+
+                HttpResponseMessage response = null;
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    response = await client.PostAsync(uri, content);
+                    string contentString = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("\nResponse:\n");
+                    Console.WriteLine(JsonPrettyPrint(contentString));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        public static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        public static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
